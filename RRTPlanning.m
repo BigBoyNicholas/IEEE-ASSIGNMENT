fad8857045f04e441980c5e2068cb89ac61e69ec
Nick <118523158+BigBoyNicholas@users.noreply.github.com>
2023-05-11 16:53:11 +0100

    Stuff

---
+++ RRTPlanning.m
@@ -0,0 +1,95 @@
+tic
+% Set RNG seed for repeatable result
+rng(1,"twister");
+thisMap = load("uavMapCityBlock.mat","omap");
+omap = thisMap.omap;
+% Consider unknown spaces to be unoccupied
+omap.FreeThreshold = omap.OccupiedThreshold;
+startLoc = [12 22 25 pi/2];
+objectiveLoc = [150 180 35 pi/2];
+figure("Name","StartAndGoal")
+hMap = show(omap);
+hold on
+scatter3(hMap,startLoc(1),startLoc(2),startLoc(3),30,"red","filled")
+scatter3(hMap,objectiveLoc(1),objectiveLoc(2),objectiveLoc(3),30,"green","filled")
+hold off
+view([-31 63])
+%Define State Space Object
+ss = ExampleHelperUAVStateSpace("MaxRollAngle",pi/6,...
+                                "AirSpeed",6,...
+                                "FlightPathAngleLimit",[-0.1 0.1],...
+                                "Bounds",[-20 220; -20 220; 10 100; -pi pi]);
+%Create a tolerance around objective
+threshold = [(objectiveLoc-0.5)' (objectiveLoc+0.5)'; -pi pi];
+setWorkspaceGoalRegion(ss,objectiveLoc,threshold)
+sv = validatorOccupancyMap3D(ss,"Map",omap);
+sv.ValidationDistance = 0.1;
+
+%Create an RRT Planner
+planner = plannerRRT(ss,sv);
+planner.MaxConnectionDistance = 50;
+planner.GoalBias = 0.10;  
+planner.MaxIterations = 400;
+planner.GoalReachedFcn = @(~,x,y)(norm(x(1:3)-y(1:3)) < 5);
+%Run the planner
+[pthObj,solnInfo] = plan(planner,startLoc,objectiveLoc);
+%If successful, create a UAV to follow path
+if (solnInfo.IsPathFound)
+    figure("Name","OriginalPath")
+    % Visualize the 3-D map
+    show(omap)
+    hold on
+    scatter3(startLoc(1),startLoc(2),startLoc(3),30,"red","filled")
+    scatter3(objectiveLoc(1),objectiveLoc(2),objectiveLoc(3),30,"green","filled")
+    
+    interpolatedPathObj = copy(pthObj);
+    interpolate(interpolatedPathObj,1000)
+    
+    % Plot the interpolated path based on UAV Dubins connections
+    hReference = plot3(interpolatedPathObj.States(:,1), ...
+        interpolatedPathObj.States(:,2), ...
+        interpolatedPathObj.States(:,3), ...
+        "LineWidth",2,"Color","g");
+    
+    % Plot simulated UAV trajectory based on fixed-wing guidance model
+    % Compute total time of flight and add a buffer
+    timeToReachGoal = 1.05*pathLength(pthObj)/ss.AirSpeed;
+    waypoints = interpolatedPathObj.States;
+    [xENU,yENU,zENU] = exampleHelperSimulateUAV(waypoints,ss.AirSpeed,timeToReachGoal);
+    hSimulated = plot3(xENU,yENU,zENU,"LineWidth",2,"Color","r");
+    legend([hReference,hSimulated],"Reference","Simulated","Location","best")
+    hold off
+    view([-31 63])
+end
+if (solnInfo.IsPathFound)
+    smoothWaypointsObj = exampleHelperUAVPathSmoothing(ss,sv,pthObj);
+    
+    figure("Name","SmoothedPath")
+    % Plot the 3-D map
+    show(omap)
+    hold on
+    scatter3(startLoc(1),startLoc(2),startLoc(3),30,"red","filled")
+    scatter3(objectiveLoc(1),objectiveLoc(2),objectiveLoc(3),30,"green","filled")
+    
+    interpolatedSmoothWaypoints = copy(smoothWaypointsObj);
+    interpolate(interpolatedSmoothWaypoints,1000)
+    
+    % Plot smoothed path based on UAV Dubins connections
+    hReference = plot3(interpolatedSmoothWaypoints.States(:,1), ...
+        interpolatedSmoothWaypoints.States(:,2), ...
+        interpolatedSmoothWaypoints.States(:,3), ...
+        "LineWidth",2,"Color","g");
+    
+    % Plot simulated flight path based on fixed-wing guidance model
+    waypoints = interpolatedSmoothWaypoints.States;
+    timeToReachGoal = 1.05*pathLength(smoothWaypointsObj)/ss.AirSpeed;
+    [xENU,yENU,zENU] = exampleHelperSimulateUAV(waypoints,ss.AirSpeed,timeToReachGoal);
+    hSimulated = plot3(xENU,yENU,zENU,"LineWidth",2,"Color","r");
+    
+    legend([hReference,hSimulated],"SmoothedReference","Simulated","Location","best")
+    hold off
+    view([-31 63]);
+end
+planeSpeed = string(timeToReachGoal);
+disp(planeSpeed);
+toc
